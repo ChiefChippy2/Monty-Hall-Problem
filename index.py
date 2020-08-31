d7a99e778cf6e0e66410fa6d38e6812f54414d47
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-08-31 23:01:04 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 import random
+true=True
+false=False
 def start():
   doors=[0,0,1] 
   choice,door,leftover=random.sample([0,1,2],3)
@@ -7,10 +9,20 @@ def start():
   print("You chose door #"+str(choice)+", host opened door #"+str(door))
   if(random.randint(0,1)==0):
     print("Swapped")
+    isSwap=true;
     choice,leftover=swap(choice,leftover)
   print("Choice is -> "+str(doors[choice]))
+  return [isSwap,doors[choice]]
 def swap(x,y):
   return [y,x]
+unswap=0
+swap=0
 for i in range(10):
-  start()
+  result=start()
+  if(result[1]!=2):
+    continue
+  if(result[0]):
+    unswap+=1
+  else:
+    swap+=1
   
