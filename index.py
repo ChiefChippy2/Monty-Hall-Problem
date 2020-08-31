7365881fb3b78232e68caf1d87135eb29e843715
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-08-31 22:47:30 +0200

    Create index.py

---
+++ index.py
@@ -0,0 +1,12 @@
+import random
+def start():
+  doors=[0,0,1] /*1 is the jackpot*/
+  choice,door,leftover=random.sample([0,1,2],3)
+  if(door==2):
+    choice,door=swap(choice,door)
+  if(random.randint(0,1)==0):
+    choice,leftover=swap(leftover,choice)
+  print("Choice is -> "+doors[choice])
+start()
+  
+  
