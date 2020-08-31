764f34c567539cb033ade3a13c9d63a96894738d
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-08-31 22:55:32 +0200

    Update index.py

--- index.py
+++ index.py
@@ -4,10 +4,13 @@ def start():
   choice,door,leftover=random.sample([0,1,2],3)
   if(door==2):
     choice,door=swap(choice,door)
+  print("You chose door #"+str(choice)+", host opened door #"+str(door))
   if(random.randint(0,1)==0):
+    print("Swapped")
     choice,leftover=swap(leftover,choice)
   print("Choice is -> "+str(doors[choice]))
 def swap(x,y):
   return [y,x]
-start()
+for i in range(10):
+  start()
   
