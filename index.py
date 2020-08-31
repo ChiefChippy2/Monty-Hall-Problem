e148f1bf1ddac76b166abf1fb1150cd766e21dd9
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-08-31 23:02:16 +0200

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ def start():
   print("You chose door #"+str(choice)+", host opened door #"+str(door))
   if(random.randint(0,1)==0):
     print("Swapped")
-    isSwap=true;
+    isSwap=true
     choice,leftover=swap(choice,leftover)
   print("Choice is -> "+str(doors[choice]))
   return [isSwap,doors[choice]]
